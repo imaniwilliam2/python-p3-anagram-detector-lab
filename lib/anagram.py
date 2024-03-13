57f2a5c8aac90703be43002afcf360edd2e636f0
imaniwilliam2 <154285022+imaniwilliam2@users.noreply.github.com>
2024-03-13 12:06:55 -0400

    test passing

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,7 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, words):
+        words = [w.lower() for w in words]
+        return [w for w in words if sorted(w) == sorted(self.word) and w != self.word]
